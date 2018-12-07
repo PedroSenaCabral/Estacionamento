f2497c37d7c042f7c0a4dc1c43a53e503c8244d1
dennisferreira1 <dennisferreira1@hotmail.com>
2018-12-07 14:21:05 -0300

    formatando

--- src/veiculo.cpp
+++ src/veiculo.cpp
@@ -10,15 +10,15 @@ Veiculo::~Veiculo(){}
 
 string Veiculo::getCor() { return this->cor; };
 string Veiculo::getModelo() { return this->modelo; };
-int    Veiculo::getRodas() {return this->rodas; };
-int    Veiculo::getAno() { return this->ano; };
-int    Veiculo::getMaxPassageiros() { return this->maxPassageiros; };
+   int Veiculo::getRodas() {return this->rodas; };
+   int Veiculo::getAno() { return this->ano; };
+   int Veiculo::getMaxPassageiros() { return this->maxPassageiros; };
 
-void   Veiculo::setCor(std::string cor) { this->cor = cor;}
-void   Veiculo::setModelo(std::string modelo) { this->modelo = modelo;}
-void   Veiculo::setAno(int ano) { this->ano = ano;}
-void   Veiculo::setRodas(int quantidade) { this->rodas = quantidade;}
-void   Veiculo::setMaxPassageiros(int maxPassageiros) { this->maxPassageiros = maxPassageiros;}
+  void Veiculo::setCor(std::string cor) { this->cor = cor;}
+  void Veiculo::setModelo(std::string modelo) { this->modelo = modelo;}
+  void Veiculo::setAno(int ano) { this->ano = ano;}
+  void Veiculo::setRodas(int quantidade) { this->rodas = quantidade;}
+  void Veiculo::setMaxPassageiros(int maxPassageiros) { this->maxPassageiros = maxPassageiros;}
 
 ostream& operator<< (ostream& ostr, Veiculo* v)
 {
