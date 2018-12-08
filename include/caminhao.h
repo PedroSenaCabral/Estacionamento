dc76a575cd15addb9471dbc7c04bf8a7b3720300
Victor bittencourt <victor.bmv@hotmail.com>
2018-12-07 23:48:44 -0300

    Classe Caminhao

---
+++ include/caminhao.h
@@ -0,0 +1,101 @@
+/**
+ * @file    caminhao.h
+ * @brief   Definição da classe Caminhao em C++
+ * @author  Victor Bittencourt Mafra Veríssimo (victor.bmv15@gmail.com)
+ * @since   06/12/2018
+ * @date    07/12/2018
+ * @sa      veiculo.h
+ */
+#ifndef _CAMINHAO_H
+#define _CAMINHAO_H
+
+#include <iostream>
+#include "veiculo.h"
+#include <string>
+
+using namespace std;
+/**
+* A classe Caminhao armazena um conjunto de valores, permitindo utilizá-los quando necessário.
+*
+*/
+class Caminhao: public Veiculo{
+private:
+  int m_quant_eixos; /**< Define a quantidade de eixos do caminhao. */
+  float m_peso_carga; /**< Define o peso da carga do caminhao. */
+
+public:
+  /**
+  * Contrutor padrão. Constrói um objeto com valores default.
+	*
+  */
+  Caminhao::Caminhao();
+  /**
+  * Contrutor parametrizado. Constrói um objeto apartir dos valores passados no construtor.
+	* @param cor_ O atributo cor que será armazenado.
+	* @param modelo_ O atributo modelo que será armazenado.
+  * @param placa_ O atributo placa que será armazenado.
+  * @param tipo_ O atributo tipo que será armazenado.
+  * @param rodas_ O atributo rodas que será armazenado.
+  * @param ano_ O atributo ano que erá armazenado.
+  * @param maxPassageiros_ O atributo Max Passageiros que será armazenado.
+  * @param eixos_ O atributo eixos que será armazenado.
+  * @param peso_ O atributo peso que será armazenado.
+  */
+  Caminhao(std::string cor_, std::string modelo_, std::string placa_, std::string tipo_, int rodas_, int ano_, int maxPassageiros_, int eixos_, float peso_);
+  /**
+  * Destrutor.
+  */
+  ~Caminhao ();
+  /**
+  * Método que retorna o tipo do objeto.
+  * @return string
+  */
+  string getTipo();
+  /**
+  * Método que retorna a quantidade de eixos.
+  * @return int
+  */
+  int getEixo();
+  /**
+  * Método que retorna o peso do caminhao.
+  * @return float
+  */
+  float getPeso();
+  /**
+  * Sobrecarga do método getConsumoPorKm da classe Veiculo. Retorna o consumo de combustível por Km.
+  *
+  * @return double
+  */
+  double getConsumoPorKM();
+  /**
+  * Método que atualiza o peso da carga do caminhao.
+  *
+  */
+  void setPeso(float peso_);
+  /**
+  * Método que sobrecarrega o método print da classe Veiculo. Imprime os dados do caminhao.
+  *
+  */
+  void print(ostream& cout);
+  /**
+  * Método que sobrecarrega o método put da classe Veiculo. Recebe os dados do caminhao.
+  *
+  */
+  void put(istream& cin);
+  /**
+	* Sobrecarga do operador de extração <<.
+	* @return ostream Retorna uma referência para um objeto ostream
+	*/
+  friend std::istream& operator<<(std::istream & os, Caminhao *v);
+  /**
+	* Sobrecarga do operador de inserção <<.
+	* @return istream Retorna uma referência para um objeto ostream
+	*/
+  friend istream& operator>>(std::istream& istr, Caminhao *v);
+
+};
+
+
+
+
+#endif
---
+++ src/caminhao.cpp
@@ -0,0 +1,71 @@
+#include "caminhao.h"
+
+using namespace std;
+
+Caminhao::Caminhao(string cor, string modelo, string placa, string tipo, int rodas, int ano, int maxPassageiros,int eixos, float peso):
+  Veiculo(cor, modelo, placa, tipo, rodas, ano, maxPassageiros),
+  m_quant_eixos(eixos),m_peso_carga(peso){}
+
+
+Caminhao::Caminhao()
+{
+   Veiculo("Nao definido", "Nao definido", "OOOO-0000", "Nao definido", -1, -1, -1);
+   this->m_peso_carga = -1;
+   this->m_quant_eixos = -1;
+}
+
+Caminhao::~Caminhao(){}
+
+int Caminhao::getEixo()
+{
+  return this->m_quant_eixos;
+}
+
+float Caminhao::getPeso()
+{
+  return this->m_peso_carga;
+}
+
+double Caminhao::getConsumoPorKM()
+{
+  return 8.5;
+}
+
+string Caminhao::getTipo()
+{
+  return "Caminhao";
+}
+
+void Caminhao::setPeso(float peso_)
+{
+  this->m_peso_carga = peso_;
+}
+void Caminhao::print(ostream& cout)
+{
+    cout<< "Cor: " << this->m_cor << std::endl
+        << "Modelo: " << this->m_modelo << std::endl
+        << "Placa: " << this->m_placa << std::endl
+        << "Número de rodas: " << this->m_rodas << std::endl
+        << "Ano: " << this->m_ano << std::endl
+        << "Máx Passageiros: " << this->m_maxPassageiros << std::endl
+        << "Quantidade de Eixo: " << this->m_quant_eixos << std::endl
+        << "Peso da carga : " << this->m_quant_eixos << std::endl
+        << "Consumo Km/l : " << this->getConsumoPorKM()<< std::endl;
+}
+
+void Caminhao::put(istream& cin)
+{
+  cin >> this->m_cor >> this->m_modelo >> this->m_placa >> this->m_rodas >> this->m_ano >> this->m_maxPassageiros >> this->m_quant_eixos >> this->m_peso_carga;
+}
+
+ostream& operator<< (ostream& ostr, Caminhao* v)
+{
+    v->print(ostr);
+    return ostr;
+}
+
+istream& operator>> (istream& istr, Caminhao* v)
+{
+	v->put(istr);
+	return istr;
+}
